c3a2cd19aae6018e5a1327639fe85919b13e32ce
Sthefani Laura <slauraic@hotmail.com>
2018-10-17 22:25:15 -0300

    Formatar tweet

--- pasta/script.js
+++ pasta/script.js
@@ -1,23 +1,27 @@
 $(document).ready(function (){
-$("#buttonSubmit").click( function(event){
-  event.preventDefault();
-  $("#tweet").val();
-  insertTweet();
+  $("#buttonSubmit").click( function(event){
+    event.preventDefault();
+    $("#tweet").val();
+    insertTweet();
 
-});
+  });
 
-function insertTweet (){
-  $("#tweets").prepend(`
-    <p id="newTweet"> ${$("#tweet").val()} </p>
-    `)
-}
 
-function charCounter (){
-  let charLeft = 280 - $("#tweet").val().length;
-  return $("#counter").text(`Você ainda tem ${charLeft} caracteres`)
-}
+  function insertTweet (){
+    $("#tweets").prepend(`
+      <p class="bg-warning p-5" id="newTweet"> ${$("#tweet").val()} </p>
+      `)
+  }
+
+  function charCounter (){
+    let charLeft = 280 - $("#tweet").val().length;
+    return $("#counter").text(`Você ainda tem ${charLeft} caracteres`)
+  }
+
   $("#tweet").keyup(function(){
     charCounter();
   });
+
+
 
 });
